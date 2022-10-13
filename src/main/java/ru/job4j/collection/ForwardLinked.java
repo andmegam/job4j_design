fe7781c23749a0b8ba1ffa577b511549e9d9afcd
andmmm <andmmm@mail.ru>
2022-10-13 22:17:56 +0400

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -5,16 +5,19 @@ import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
 
+    private int size;
     private Node<T> head;
 
     public void addFirst(T value) {
         head = new Node<>(value, head);
+        size++;
     }
 
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
             head = node;
+            size++;
             return;
         }
         Node<T> tail = head;
@@ -22,6 +25,7 @@ public class ForwardLinked<T> implements Iterable<T> {
             tail = tail.next;
         }
         tail.next = node;
+        size++;
     }
 
     public T deleteFirst() {
@@ -31,7 +35,12 @@ public class ForwardLinked<T> implements Iterable<T> {
         Node<T> deleteNode = head;
         head = head.next;
         deleteNode.next = null;
+        size--;
         return deleteNode.value;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     @Override
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -1,7 +1,5 @@
 package ru.job4j.collection;
 
-import java.util.Iterator;
-
 public class SimpleStack<T> {
 
     private ForwardLinked<T> linked = new ForwardLinked<T>();
@@ -12,5 +10,9 @@ public class SimpleStack<T> {
 
     public void push(T value) {
         linked.addFirst(value);
+    }
+
+    public boolean isEmpty() {
+        return linked.isEmpty();
     }
 }
